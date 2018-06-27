d1735782fe63a84c4a2182e43a37c808fbd70ae3
David <weixin.xiao@cn.alps.com>
2018-06-27 15:21:08 +0800

    c++ console application

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,14 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+
+	std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
+
